5ccbf32a8792ff2793b23b2c45b7385365b702e2
yan cesar <yancesar21@gmail.com>
2024-04-11 21:21:38 -0300

    refactoring the code and do some changes in css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
 import Form from './Form';
 
-import './App.css';
+import "./Form.css" 
 
 function App() {
   return (
     <>
-    <div className='form-components'>
       <div className='sign-container'>
-          <h1>Sign in</h1>
-        <div className='form-container'>
           <Form></Form> 
-        </div>
       </div>
-    </div>
     </>
   );
 }
